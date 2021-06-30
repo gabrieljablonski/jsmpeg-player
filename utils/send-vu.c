7ac62ddd7dd36cd5d0bc4f6f60446037c400eafa
gabrieljablonski <gabriel.g.jablonski@gmail.com>
2021-06-30 17:30:50 -0300

    never close connection

--- utils/send-vu.c
+++ utils/send-vu.c
@@ -16,9 +16,85 @@
 #define TS_HEADER_SIZE 4
 #define TS_PACKET_SIZE 188
 
+int http_sock = -1;
 int ts_payload = 0;
 uint8_t ts_continuity_counter = 0x0;
 uint8_t ts_packet[TS_PACKET_SIZE];
+
+void http_connect(char *host, int port) {
+  http_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (http_sock < 0) {
+    fprintf(stderr, "failed to create socket %d\n", http_sock);
+    exit(-1);
+  }
+  struct hostent *server = gethostbyname(host);
+  if (!server) {
+    fprintf(stderr, "host not found\n");
+    exit(-1);
+  }
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+  memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+  if (connect(http_sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+    fprintf(stderr, "failed to connect to server\n");
+    exit(-1);
+  }
+}
+
+void http_write(char *data, int len) {
+  int sent = 0;
+  do {
+    int bytes = write(http_sock, data + sent, len - sent);
+    if (bytes < 0) {
+      fprintf(stderr, "http request failed\n");
+      exit(-1);
+    }
+    if (bytes == 0) {
+      break;
+    }
+    sent += bytes;
+  } while (sent < len);
+}
+
+void http_write_payload(char *data, int len) {
+  char payload[10240];
+  snprintf(payload, 32, "%x", len);
+  int offset = strlen(payload);
+  memcpy(payload + offset, "\r\n", 2);
+  offset += 2;
+  memcpy(payload + offset, data, len);
+  offset += len;
+  memcpy(payload + offset, "\r\n", 2);
+  offset += 2;
+  http_write(payload, offset);
+}
+
+int http_get_response(char *response) {
+  memset(response, 0, sizeof(response));
+  int total = sizeof(response) - 1;
+  int received = 0;
+  do {
+    int bytes = read(http_sock, response + received, total - received);
+    if (bytes < 0) {
+      fprintf(stderr, "http parse response failed\n");
+      exit(-1);
+    }
+    if (bytes == 0) {
+      break;
+    }
+    received += bytes;
+  } while (received < total);
+  return received;
+}
+
+void http_write_header(char *host, char *path) {
+  char *http_header = "POST %s HTTP/1.1\r\nTransfer-Encoding: chunked\r\nUser-Agent: xcoder\r\nAccept: /\r\nConnection: close\r\nHost: %s\r\nIcy-MetaData: 1\r\n\r\n";
+  char message[10240];
+  snprintf(message, 10249, http_header, path, host);
+  http_write(message, strlen(message));
+}
 
 void send_packet(int pid, char *host, int port, char *path) {
   ts_packet[3] = ts_continuity_counter | 0x30; /* continuity counter, no scrambling, adaptation field and payload */
@@ -31,84 +107,7 @@ void send_packet(int pid, char *host, int port, char *path) {
   for (int i = TS_HEADER_SIZE + 2; i < TS_PACKET_SIZE - ts_payload; ++i) { /* pad the packet */
     ts_packet[i] = 0xFF;
   }
-
-  char *http_header = "POST %s HTTP/1.1\r\nTransfer-Encoding: chunked\r\nUser-Agent: xcoder\r\nAccept: /\r\nConnection: close\r\nHost: localhost\r\nIcy-MetaData: 1\r\n\r\n";
-  char message[10240];
-
-  snprintf(message, 10249, http_header, path);
-  int base_len = strlen(message);
-
-  // 188 bytes
-  message[base_len] = 0x62;
-  message[base_len + 1] = 0x63;
-  message[base_len + 2] = '\r';
-  message[base_len + 3] = '\n';
-  memcpy(message + base_len + 4, ts_packet, TS_PACKET_SIZE);
-
-  // indicate message ended ('transfer-encoding: chunked' requirement)
-  message[base_len + TS_PACKET_SIZE + 4] = '\r';
-  message[base_len + TS_PACKET_SIZE + 5] = '\n';
-  message[base_len + TS_PACKET_SIZE + 6] = 0x30;
-  message[base_len + TS_PACKET_SIZE + 7] = '\r';
-  message[base_len + TS_PACKET_SIZE + 8] = '\n';
-  message[base_len + TS_PACKET_SIZE + 9] = '\r';
-  message[base_len + TS_PACKET_SIZE + 10] = '\n';
-
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-  if (sockfd < 0) {
-    fprintf(stderr, "failed to create socket %d\n", sockfd);
-    exit(-1);
-  }
-  
-  struct hostent *server = gethostbyname(host);
-  if (!server) {
-    fprintf(stderr, "host not found\n");
-    exit(-1);
-  }
-
-  struct sockaddr_in serv_addr;
-  memset(&serv_addr, 0, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(port);
-  memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
-    fprintf(stderr, "failed to connect to server\n");
-    exit(-1);
-  }
-
-  int total = base_len + TS_PACKET_SIZE + 11;
-  int sent = 0;
-  do {
-    int bytes = write(sockfd, message + sent, total - sent);
-    if (bytes < 0) {
-      fprintf(stderr, "http request failed\n");
-      exit(-1);
-    }
-    if (bytes == 0) {
-      break;
-    }
-    sent += bytes;
-  } while (sent < total);
-
-  char response[10240];
-  memset(response, 0, sizeof(response));
-  total = sizeof(response) - 1;
-  int received = 0;
-  do {
-    int bytes = read(sockfd, response + received, total - received);
-    if (bytes < 0) {
-      fprintf(stderr, "http parse response failed\n");
-      exit(-1);
-    }
-    if (bytes == 0) {
-      break;
-    }
-    received += bytes;
-  } while (received < total);
-
-  close(sockfd);
+  http_write_payload(ts_packet, TS_PACKET_SIZE);
   ts_payload = 0;
 }
 
@@ -156,6 +155,9 @@ int main(int argc, char **argv)
     fclose(vu_file);
 		return -3;
 	}
+
+  http_connect(host, port);
+  http_write_header(host, path);
 
   while (1) {
 	  ts_packet[0] = 0x47; /* sync byte */ 
